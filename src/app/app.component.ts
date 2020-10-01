2ddfc9f7d28d4047b8bdc2bf6bc3d423505fcedf
Anshul <guptaanshul1808@gmail.com>
2020-10-02 01:04:16 +0530

    first basic working commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,30 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'resume-anshul-angular';
+
+  //dynamically loading scripts
+  constructor() { 
+    this.loadScripts(); 
+  } 
+ 
+  // Method to dynamically load JavaScript 
+  loadScripts() { 
+ 
+    // This array contains all the files/CDNs 
+    const dynamicScripts = [ 
+    "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js",
+    "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js",
+    '../../../assets/js/scripts.js'
+    ]; 
+    for (let i = 0; i < dynamicScripts.length; i++) { 
+      const node = document.createElement('script'); 
+      node.src = dynamicScripts[i]; 
+      node.type = 'text/javascript'; 
+      node.async = false; 
+      document.getElementsByTagName('head')[0].appendChild(node); 
+    } 
+  }
 }
